c52a0cf32dcee3d25b6719db7e1941cfaa85a2ec
Sprudeel <79663902+Sprudeel@users.noreply.github.com>
2021-10-02 10:20:51 +0200

    v1.1

--- source.js
+++ source.js
@@ -1,22 +1,40 @@
-
-
 /////////////////////////////////
 //             GAME            //
 /////////////////////////////////
 
 
-var mode;
-var subject;
+// Random Int for random question
 var randomint;
+
+// Array containing the answers
 var arrayAnswers = [];
-var currentAnswer;
+
+// Var of currentAnswer
+var currentAnswer = "noch nicht definiert ;)"
+
+// Last Random Int (no repeats)
 var last = 1;
+
+// Var that tells if it started
 var started = false;
+
+// Var for Timer
 var going = false;
+
+// Correct Counter
 var correct = 0;
+
+// Max Questions for Learn Mode
 var length;
+
+// Vars for Button Delay
 var delay = 1000;
 var lastClick = 0;
+
+
+// Subject
+var subject;
+var mode;
 
 
 
@@ -99,41 +117,111 @@ if(location.hash == "") {
 
 
 function setMode(inputmode, buttonId) {
-    resetTimer();
-    counter = 1;
-    document.getElementById(500).innerHTML = "<p id='502' style='color: red; text-align: center;'>keine</p><p id=501></p>";
 
-    // set all button to normal
+    
+
+    // Reset Timer on Left Side
+    resetTimer();
+
+    // set Correct counter back to 1
+    counter = 1;
+    correct = 0;
+
+    // Reset Correctly Solved List
+    document.getElementById(500).innerHTML = "<p id='502' style='color: red; text-align: center;'>keine</p><p id=501></p>";
+    document.getElementById(333).innerHTML = "Korrekt gelöst";
+
+    // set all buttons to normal
     document.getElementById(101).style.backgroundColor = "white";
     document.getElementById(102).style.backgroundColor = "white";
     document.getElementById(103).style.backgroundColor = "white";
 
+    // fill clicked button
     document.getElementById(buttonId).style.backgroundColor = "#6cb104";
+
     // set the mode in var
     mode = inputmode;
 
+    // Start game if a Subject has been selected
     if(subject != null) {
         Game();
     }
 
+    // Reset Game Started Value if Game has Started
     if(started == true) {
-        startes = false;
+        started = false;
     }
-    return mode, started;
+
+    // Start Section for Speed Mode
+    if(started === false && mode === "speed" && subject != null) {
+
+        // Set Started to True
+        started = true;
+
+        // Hide Menu
+        hideMenu();
+
+        // Hide Timer Controls
+        document.getElementById(555).style.display = "none";
+        document.getElementById(556).style.display = "none";
+
+        // Ste Text Overlay
+        document.getElementById("overlay").style.display = "block";
+        document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>3</p>";
+
+
+        // display 3
+        setTimeout(() => {
+            document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>2</p>";
+        }, 1000);
+
+        // display 2
+        setTimeout(() => {
+            document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>1</p>";
+        }, 2000);
+
+        // display 1
+        setTimeout(() => {
+            document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>GO!</p>";
+        }, 3000);
+
+        // display GO
+        setTimeout(() => {
+            document.getElementById("overlay").style.display = "none";
+            startTimer();
+        }, 3300);
+
+        
+    }
+
+    
+    // Return the selected Mode and if game started
+    return mode, started, arrayAnswers;
 }
 
 function setSubject(inputsubject, buttonId) {
-    resetTimer();
-    counter = 1;
-    document.getElementById(500).innerHTML = "<p id='502' style='color: red; text-align: center;'>keine</p><p id=501></p>";
-    for (let i = 1; i < 8; i++) {
-        document.getElementById("20" + i).style.backgroundColor = "white";
-    }
+    
+     // Reset Timer on Left Side
+     resetTimer();
 
-    document.getElementById(buttonId).style.backgroundColor = "#2d73f3";
-
-    // set the subject in var
-    subject = inputsubject;
+     // Reset Correct Counter
+     counter = 1;
+     correct = 0;
+ 
+     // Reset Correctly Solved List
+     document.getElementById(500).innerHTML = "<p id='502' style='color: red; text-align: center;'>keine</p><p id=501></p>";
+ 
+     // Set all Buttons back to normal
+     for (let i = 1; i < 8; i++) {
+         document.getElementById("20" + i).style.backgroundColor = "white";
+     }
+ 
+     // Fill Clicked Button
+     document.getElementById(buttonId).style.backgroundColor = "#2d73f3";
+ 
+     // set the subject in var
+     subject = inputsubject;
+ 
 
     if (location.hash === "#australien") {
         if(document.getElementById(2004).checked) {
@@ -241,21 +329,63 @@ function setSubject(inputsubject, buttonId) {
 
 
     
+    
+    // Set Length var for Correctly Solved Board
     length = arrayAnswers.length;
     
 
-
+    // Start Game if Mode has been set
     if(mode != null) {
         Game();
     }
 
     
 
-    started = false;
+    // Reset Game Started Value if Game has Started
+    if(started == true) {
+        started = false;
+    }
+
+    if(started === false && mode === "speed" && subject != null) {
+        started = true;
+        hideMenu();
+        document.getElementById(555).style.display = "none";
+        document.getElementById(556).style.display = "none";
+        document.getElementById("overlay").style.display = "block";
+        document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>3</p>";
+
+        setTimeout(() => {
+            document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>2</p>";
+        }, 1000);
+
+        setTimeout(() => {
+            document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>1</p>";
+        }, 2000);
+
+        setTimeout(() => {
+            document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>GO!</p>";
+        }, 3000);
+
+        setTimeout(() => {
+            document.getElementById("overlay").style.display = "none";
+            startTimer();
+        }, 3300);
+
+        
+    }
+    
+
+    // Return Values
     return subject, arrayAnswers, started, length;
 }
-
+var aaaa;
 function Game() {
+    
+    if(hnujzhj == true) {
+        window.alert("2te Hürde gemeistert!");
+        aaaa = true;
+    }
+    // Show Timer Controls
     document.getElementById(555).style.display = "inline-block";
     document.getElementById(556).style.display = "inline-block";
 
@@ -268,17 +398,23 @@ function Game() {
     document.getElementById(420).style.boxShadow = "none";
     document.getElementById(420).value = "";
 
+
+
+
     // ENDLESS MODE
     if(mode === "endless") {
 
+        // Set Loading Image
         if(location.hash === "#australien") {
         document.getElementById(1000).src = "images/loading.jpg";
         } else {
             document.getElementById(1000).src = "images/loading2.jpg";
         }
         
+        // Choose Random Answer
         randomNumber = Math.floor(Math.random() * arrayAnswers.length + 1);
 
+        // Make Sure it does not repeat itself ;)
         if(randomNumber === last) {
             randomNumber = Math.floor(Math.random() * arrayAnswers.length + 1);
 
@@ -286,8 +422,16 @@ function Game() {
                 randomNumber = Math.floor(Math.random() * arrayAnswers.length + 1);
             }
         }
+
+
+
+        // Set current Answers Var for CheckSolution
         currentAnswer = arrayAnswers[randomNumber - 1];
+
+        // Set Last Var to new Answers for next run
         last = randomNumber;
+
+        // Set Picture Name Var to change ä ü ö for more compatability!
         let picturename = currentAnswer;
 
         // change ae oe and ue to ä ö and ü
@@ -300,6 +444,7 @@ function Game() {
         } 
 
 
+        // Set Image
         setTimeout(() => {
             if(location.hash == "#australien") {
                 document.getElementById(1000).src = "images/" + subject + "/" +  picturename + ".jpg"
@@ -316,42 +461,22 @@ function Game() {
 
     } else if(mode == "speed") {
 
-        // START SCREEN
-        if(started == false) {
-            hideMenu();
-            document.getElementById(555).style.display = "none";
-            document.getElementById(556).style.display = "none";
-            document.getElementById("overlay").style.display = "block";
-            document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>3</p>";
-
-            setTimeout(() => {
-                document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>2</p>";
-            }, 1000);
-
-            setTimeout(() => {
-                document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>1</p>";
-            }, 2000);
-
-            setTimeout(() => {
-                document.getElementById("overlay").innerHTML = "<p class='textoverlay2'>GO!</p>";
-            }, 3000);
-
-            setTimeout(() => {
-                document.getElementById("overlay").style.display = "none";
-                startTimer();
-            }, 3300);
-
-            started = true;
-        }
-
+        
+        // Hide Timer Controls
+        document.getElementById(555).style.display = "none";
+        document.getElementById(556).style.display = "none";
+               
+        // Set Loading Image
         if(location.hash === "#australien") {
             document.getElementById(1000).src = "images/loading.jpg";
         } else {
             document.getElementById(1000).src = "images/loading2.jpg";
         }
         
+        // Choose Random Answer
         randomNumber = Math.floor(Math.random() * arrayAnswers.length + 1);
 
+        // Make Sure it does not repeat itself ;)
         if(randomNumber === last) {
             randomNumber = Math.floor(Math.random() * arrayAnswers.length + 1);
 
@@ -359,8 +484,15 @@ function Game() {
                 randomNumber = Math.floor(Math.random() * arrayAnswers.length + 1);
             }
         }
+
+
+        // Set current Answers Var for CheckSolution
         currentAnswer = arrayAnswers[randomNumber - 1];
+
+        // Set Last Var to new Answers for next run
         last = randomNumber;
+
+        // Set Picture Name Var to change ä ü ö for more compatability!
         let picturename = currentAnswer;
 
         // change ae oe and ue to ä ö and ü
@@ -372,7 +504,7 @@ function Game() {
             picturename = picturename.replace("ü", "ue");
         } 
 
-
+        // Set Image
         setTimeout(() => {
             if(location.hash === "#australien") {
                 document.getElementById(1000).src = "images/" + subject + "/" +  picturename + ".jpg"
@@ -382,7 +514,9 @@ function Game() {
         }, 200); 
         
 
-        
+        // Start Var to True
+        started = true;
+
 
         return started, last, randomNumber, currentAnswer;
 
@@ -391,7 +525,7 @@ function Game() {
     } else if (mode == "learn") {
         
 
-
+        // check if everything has already been learned
         if(correct == length) {
             if(location.hash == "#australien") {
                 document.getElementById(1000).src = "images/finished.jpg"
@@ -402,9 +536,12 @@ function Game() {
             return last, randomNumber, currentAnswer;
         }
 
+        // Update Correct Counter
         document.getElementById(333).innerHTML = "Korrekt gelöst " + correct + "/" + length;
 
 
+
+        // Set Loading Image
         if(location.hash === "#australien") {
             document.getElementById(1000).src = "images/loading.jpg";
             } else {
@@ -414,8 +551,17 @@ function Game() {
         randomNumber = Math.floor(Math.random() * arrayAnswers.length + 1);
 
 
+        // Generate Random Question
+        randomNumber = Math.floor(Math.random() * arrayAnswers.length + 1);
+
+
+        // Set current Answers Var for CheckSolution
         currentAnswer = arrayAnswers[randomNumber - 1];
+
+        // Set Last Var to new Answers for next run
         last = randomNumber;
+
+        // Set Picture Name Var to change ä ü ö for more compatability!
         let picturename = currentAnswer;
 
         // change ae oe and ue to ä ö and ü
@@ -426,8 +572,12 @@ function Game() {
         } else if(picturename.includes("ü")) {
             picturename = picturename.replace("ü", "ue");
         } 
+
+        // Set Started var to true
         started = true;
 
+
+        // Set Question Image
         setTimeout(() => {
             if(location.hash == "#australien") {
                 document.getElementById(1000).src = "images/" + subject + "/" +  picturename + ".jpg"
@@ -445,6 +595,11 @@ function Game() {
 // Check if Solution is Correct
 function checkSolution() {
 
+    if(jjjj > 4 && hnujzhj === true && aaaa === true && bdkj === true && document.getElementById(420).value.toLowerCase() === "carlo") {
+        window.alert("Carlo Mode Activated!")
+        document.getElementById(6000).innerHTML = "Carlo Mode";
+        document.getElementById(0).style.backgroundImage = "url('images/funky.jpg')"
+    }
     // If answer is correct go here
     if(currentAnswer.toLowerCase() == document.getElementById(420).value.toLowerCase()) {
         
@@ -462,6 +617,8 @@ function checkSolution() {
         // Show Correct Answer on Correct Answer Board
         showCorrect(currentAnswer);
 
+
+       
         // Update Correct Var
         correct++;
 
@@ -488,12 +645,18 @@ function checkSolution() {
 
 
 function showSolution() {
-    // remove button
     document.getElementById(222).className = "text";
     document.getElementById(222).innerHTML = currentAnswer;
 }
+var jjjj = 0;
+function IAmHere() {
 
-
+    if(jjjj > 4 && hnujzhj === true && aaaa === true && bdkj === true) {
+        window.alert("4 Cool");
+    } else {
+        jjjj++;
+    }
+}
 
 
 
@@ -502,9 +665,14 @@ function showSolution() {
 
 document.getElementById(400).style.display = "none";
 document.getElementById(700).style.display = "none";
-
+var bdkj;
 // hide Menu
 function hideMenu() {
+
+    if(hnujzhj === true && aaaa === true) {
+        window.alert("3te Hürde gemeistert!");
+        bdkj = true;
+    }
     var menu = document.getElementById(99);
     var animation = [
         {transform: "translateX(-100%)"}
@@ -608,8 +776,16 @@ setInterval(() => {
     document.getElementById(600).innerHTML = displaydate;
     
 }, 10);
-
-
+var ll = 0;
+var hnujzhj = false
+function whyamIHere() {
+    if(ll > 5) {
+        window.alert("Carlo Mode 1te Aktivierungs Hürde Überwunden!");
+        hnujzhj = true
+    } else {
+        ll++;
+    }
+}
 
 
 
@@ -666,16 +842,46 @@ function timerCycle() {
         if(min == 1) {
             stoptime = true;
             going = false;
-            document.getElementById("overlay").style.display = "block";
-            document.getElementById("overlay").innerHTML = "<p class='textoverlay'>Ende!<span class='material-icons md-18 '>celebration</span><br>Korrekt gelöst: " + correct + " <button class='close' onclick='closeOverlay()'>Info schliessen</button> </p>" 
-            resetTimer();
-        }
+        
+
+    // MAKE ENd SCREEN 
+
+    let averagetime = (60.00 / correct).toFixed(2);
+    var aneq = "Lorem Ipsum";
+
+    document.getElementById("overlay").innerHTML = "<div class='textoverlay'><b class='end'><span class='material-icons md-18 '>celebration</span> Zeit um! <span class='material-icons md-18 '>celebration</span></b><br><br><div class='daten'>Gesammelte Daten:</div><div class='correct'>gesamte Zeit: 1min <br> Korrekt gelöst: " + correct + "<br> Ø Zeit pro Aufgabe: " + averagetime + " sec</div><br><div id='999' class='aneq'>" + aneq + "</div><button class='close' onclick='closeOverlay()'>Info schliessen</button> </div><canvas id='my-canvas'></canvas>";
+
+
+    if(correct < 6) {
+        document.getElementById(999).innerHTML = "Vielleicht solltest du noch ein wenig üben...";
+        document.getElementById(999).style.color = "crimson";
+    } else if(correct > 6 && correct < 10) {
+        document.getElementById(999).innerHTML = "Du bist auf einem guten Weg!"
+        document.getElementById(999).style.color = "orange";
+    } else if(correct > 10 && correct < 15) {
+        document.getElementById(999).innerHTML = "WOW! Du bist gut!"
+        document.getElementById(999).style.color = "green";
+    } else if(correct > 15) {
+        document.getElementById(999).innerHTML = "UNFASSBAR! Du bist unaufhaltbar!"
+        document.getElementById(999).style.color = "gold";
+    }
+    document.getElementById("overlay").style.display = "block";
+
+    if(correct > 10) {
+        var confettiSettings = { target: 'my-canvas' };
+        var confetti = new ConfettiGenerator(confettiSettings);
+        confetti.render();
+    }
+    resetTimer();
+    correct = 0;
+    return correct;
+}
     } else {
-        timer.innerHTML = hr + ':' + min + ':' + sec;
+    timer.innerHTML = hr + ':' + min + ':' + sec;
     }
 
     setTimeout("timerCycle()", 1000);
-  }
+    }
 }
 
 function resetTimer() {
